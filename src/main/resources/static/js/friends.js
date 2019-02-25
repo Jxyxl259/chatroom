c04579756b882a8ea088b411f7c88e1d9397ef3a
jiangxy9 <jiangxy9@lenovo.com>
2019-02-25 21:42:04 +0800

    update :user active tab

--- src/main/resources/static/js/friends.js
+++ src/main/resources/static/js/friends.js
@@ -17,7 +17,7 @@ var fetch_friend_list = function(){
            ' <ul style="padding-left: 20px" id="friend_list_ul" type="none">'+
            '{{each(i, _user_friend) friend}}'+
            '     <li id="${_user_friend.id}">'+
-           '        <div style="display: inline;cursor:pointer;">' +
+           '        <div style="display: inline;cursor:pointer;" onclick="show_chat_tab(this)">' +
             '           <img width="30" height="30" src="./../img/qq_icon.png"/><span>${_user_friend.userName}</span>' +
             '       </div>'+
            '     </li>'+
@@ -45,8 +45,49 @@ var fetch_friend_list = function(){
             }
         }
     })
-
-
 };
 
 
+/**
+ * 点击好友头像，弹出聊天对话框
+ * //TODO 拉取相对应的聊天记录
+ * @param ele div(用户好友头像 + name)
+ */
+var show_chat_tab = function(ele){
+
+    //先获取当前打开的活动窗口，如果有先关闭该窗口
+    var active_tab = $("#chat_record_tab_ul li.active")[0];
+    if(!isEmpty(active_tab)){
+        $(active_tab).removeClass("active");
+    }
+
+    // 获取到聊天对象昵称 ul>li>div>span
+    var _f_name_span = $(ele).children("span")[0];
+    var friend_name = _f_name_span.innerHTML;
+
+    // 获取所有的 ul>li>a 的文本，如果与即将打开的聊天对象的昵称相同则使用该聊天窗口，否则新打开一个聊天窗口
+
+
+
+    // 标签页头部信息
+    var user_tab_header =
+                ' <li class="active">'+
+                '     <a href="#'+ friend_name +'" data-toggle="tab">'+
+                        friend_name+
+                '     </a>'+
+                ' </li>';
+
+    var chat_container =
+                '<div class="tab-pane fade in active" id="'+ friend_name +'">'+
+                    '<p> 聊天记录聊天记录聊天记录聊天记录</p>'+
+                '</div>';
+
+    // 设置新的活动标签
+    $("#chat_record_tab_ul").append(user_tab_header);
+
+    // 获取当前好友的聊天记录
+    $("#chat_recoed_area_container").html("");
+    $("#chat_recoed_area_container").append(chat_container)
+
+
+};
